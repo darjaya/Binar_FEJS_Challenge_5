ea4abce1fa6455b4a91c2a5008cf280eab13f046
indar jaya <104380253+indardjaya@users.noreply.github.com>
2022-10-07 22:27:01 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import "antd/dist/antd.less";
+import "antd/dist/antd.css";
 import "slick-carousel/slick/slick.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick-theme.css";
